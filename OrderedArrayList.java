22ef2fe079699462d4d8624459094862d04aece8
lamdaham <ilam21@stuy.edu>
2020-12-09 19:09:53 -0500

    Added constructors

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,13 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 	public OrderedArrayList(int initialCapacity) {
 		super(initialCapacity);
 	}
+
+
+	public void add(T element) {
+		for (int i = 0; i<size(); i++) {
+			if (get(i)>element) {
+				super.add(i-1, element);
+			}
+		}
+	}
 }
